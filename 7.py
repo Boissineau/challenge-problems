e530734b640a8f8cbb5d47c95ed392a6f793dba4
Brendan <brendanboissineau@gmail.com>
2021-06-14 13:10:42 -0400

    https://www.codewars.com/kata/55983863da40caa2c900004e/train/python

--- 7.py
+++ 7.py
@@ -1,25 +1,50 @@
 def next_bigger(n):
     
+
     l = [x for x in str(n)]
-
-    length = len(l) - 1
-    for i in range(length):
-        if l[length - i] is not None and l[length - 1 - i] is not None:
-            if l[length - i] > l[length - 1 - i]:
-                tmp = l[length - 1 - i]
-                l[length - 1 - i] = l[length - i]
-                l[length - i] = tmp
-                break
-    
-    
-    
-    
+    tmp = l
+    length = len(l)
     num = ''
-    for i in l:
-        num += i
-    print(num)
+    idx = length - 1
+    print(n)
+    changed = False
+    while(idx != 0):
+        if l[idx - 1] is not None:
+            if l[idx] > l[idx-1]:
+                print(f'switching {l[idx-1]} with {l[idx]}')
+                t = l[idx-1]
+                tmp[idx-1] = l[idx]
+                tmp[idx] = t
+                for i in tmp:
+                    num += i
+                if int(num) > n:
+                    print(num)
+                    changed = True
+                    break
+        idx -= 1
+    if changed == False:
+        return -1
+        
+    sub = [x for x in num]
+    sub2 = sub[idx:]
+    sub2.sort()
 
-    return int(num)
+
+    for i in range(len(sub2)):
+        sub[idx + i] = sub2[i]
+    
+    ret = ''
+    for i in sub:
+        ret += i 
+
+    
+    return int(ret)
+    
+    
+    
+    
+    
+    
     
     
     # https://www.codewars.com/kata/55983863da40caa2c900004e/train/python
