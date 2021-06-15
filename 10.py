490da17086ba8edae3954eb57f055c7417ecd356
Brendan <brendanboissineau@gmail.com>
2021-06-15 11:57:51 -0400

    https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/python

---
+++ 10.py
@@ -0,0 +1,24 @@
+def reverse_words(s):
+    
+    str = []
+    tmp = ''
+    prev = False
+    for i in range(len(s)):
+        tmp += s[i]
+        if s[i] == ' ':
+            
+            str.append(tmp.strip())
+            tmp = ''
+            continue
+    str.append(tmp)
+    
+    str = str[::-1]
+    str2 = ''
+    for i in str:
+        str2 += i + ' '
+    
+    str2 = str2.strip()
+    print(str2)
+    return str2
+
+    # https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/python
