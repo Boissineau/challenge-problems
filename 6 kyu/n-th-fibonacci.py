06f5b311e6dd4f440013480956014e2ab78f4ab1
boissineau <brendanboissineau@gmail.com>
2021-06-18 11:20:07 -0400

    https://www.codewars.com/kata/522551eee9abb932420004a0/train/python

---
+++ 6 kyu/n-th-fibonacci.py
@@ -0,0 +1,9 @@
+def nth_fib(n):
+    num = []
+    num.append(0)
+    num.append(1)
+    for i in range(2, n, 1):
+        num.append(num[i-2] + num[i-1])
+    return num[n-1]
+
+    # https://www.codewars.com/kata/522551eee9abb932420004a0/train/python
