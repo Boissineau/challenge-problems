ed503ecd0b54e49e483e12210e82752548fc671c
boissineau <brendanboissineau@gmail.com>
2021-06-23 12:08:14 -0400

    added the best way to solve palindrome

--- leetcode/Easy/Palindrome/palindrome.java
+++ leetcode/Easy/Palindrome/palindrome.java
@@ -32,13 +32,18 @@ class Solution {
         
         if (val == 0) return true;
         return false;
-
-        
-
-        
-        
-        
     }
+
+    public boolean isPalindromeBetter(int x) {
+        if (x<0 || (x!=0 && x%10==0)) return false;
+        int rev = 0;
+        while (x>rev){
+            rev = rev*10 + x%10;
+            x = x/10;
+        }
+        return (x==rev || x==rev/10);
+    }
+
 }
 
 
