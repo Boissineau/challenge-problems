c336b1d35e049fb6909ccfca545c4db2124a1591
boissineau <brendanboissineau@gmail.com>
2021-06-24 21:23:11 -0400

    defang ip string

---
+++ leetcode/Easy/defangIP/defang.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String defangIPaddr(String address) {
+        String str = "";
+        for(int i = 0; i < address.length(); i++){
+            char ch = address.charAt(i);
+            if (ch == '.'){
+                str += "[.]";
+            } else {
+                str += ch;
+            }
+        }
+        return str;
+        
+    }
+}
+
+
+
+
+public class defang {
+    public static void main(String[] args){
+        
+        Solution sol = new Solution();
+        // char[] str = new char[]{'A',' ','m','a','n',',',' ','a',' ','p','l','a','n',',',' ','a',' ','c','a','n','a','l',':',' ','P','a','n','a','m','a'};
+        String ret = sol.defangIPaddr("192.168.1.1");
+        System.out.println(ret);
+    }
+
+}
